e07ecaa9e816140f5a7dc843b073ae02f33c64d4
Niroshan <niroshan05062001@gmail.com>
2022-07-16 17:36:42 +0530

    commit

---
+++ Employee/DeleteEmployee.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+export default class DeleteEmployee extends Component {
+    constructor()
+    {
+        super();
+        this.state={
+            Employee:[],
+            employeeId:'',
+            firstName:'',
+            lastName:'',
+            e_Mail:'',
+            contactNumber:'',
+            department:'',
+            dateJoined:'',
+            leaveBalance:'',
+            password:'',
+            checkPassword:''
+    }
+    this.delete=this.delete.bind(this);
+    this.handleChanges=this.handleChange.bind(this);
+}
+    delete(){
+        let employeeId=this.state.employeeId;
+        fetch("http://localhost:50734/api/EmployeeDetails/DeleteEmployee/"+employeeId,{
+            "method":"DELETE",
+            "headers": 
+            {
+                "content-type":"application/json",
+                "accept":"application/json"
+            }
+    }).then(Response=>Response.json()).then(Response=>{
+        alert("data deleted");
+})
+.catch(err=>{console.warn(err)
+})
+
+}
+handleChange(changeObject)
+    {
+        this.setState(changeObject);
+
+        
+    }
+
+    render() {
+        const {Employee}=this.state;
+        return (
+            <div className="App-header">
+          <form>
+            <table  >
+            <tr >
+                <td ><label >Employee ID</label>
+                <input className="spacer" type="text" name="employeeId" onChange={(e)=>this.handleChange({employeeId:e.target.value})}></input></td>
+                </tr>
+                <tr>
+                <td><button type="button" onClick={(e)=>this.delete(e)}>Delete</button>
+                
+                 </td>
+              
+              </tr>
+            </table>
+          
+          </form>
+          </div>
+            
+        )
+    }
+}
